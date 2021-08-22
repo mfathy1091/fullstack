5afb7657aa137333547901f8339d0114c0516d46
Mohamed Fathy <mohammad.soliman44@gmail.com>
2021-08-22 17:29:54 +0200

    first commit

---
+++ resources/js/router.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+Vue.use(Router)
+
+import firstPage from './components/pages/myFirstVuePage'
+
+const routes = [
+    {
+        path: '/my-new-vue-route',
+        component: firstPage
+    }
+];
+
+
+export default new Router({
+    mode: 'history',
+    routes
+})
